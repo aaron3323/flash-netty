af0d999bb011a56cbd8a968bf42daad869e5b78d
aaron3323 <aaron3323@msn.com>
2021-09-26 09:39:41 +0800

    update

--- src/main/java/the/flash/NettyServer.java
+++ src/main/java/the/flash/NettyServer.java
@@ -23,9 +23,17 @@ public class NettyServer {
                 .channel(NioServerSocketChannel.class)
                 .attr(AttributeKey.newInstance("serverName"), "nettyServer")
                 .childAttr(clientKey, "clientValue")
+
+                // 系统用于临时存放已完成三次握手的请求的队列的最大长度，如果连接建立频繁，服务器处理创建新连接较慢，可以适当调大这个参数
                 .option(ChannelOption.SO_BACKLOG, 1024)
+
+                // 表示是否开启TCP底层心跳机制，true为开启
                 .childOption(ChannelOption.SO_KEEPALIVE, true)
+
+                // 表示是否开启Nagle算法，true表示关闭，false表示开启，通俗地说，如果要求高实时性，有数据发送时就马上发送，就关闭，如果需要减少发送次数减少网络交互，就开启
                 .childOption(ChannelOption.TCP_NODELAY, true)
+
+
                 .childHandler(new ChannelInitializer<NioSocketChannel>() {
                     protected void initChannel(NioSocketChannel ch) {
                         System.out.println(ch.attr(clientKey).get());
